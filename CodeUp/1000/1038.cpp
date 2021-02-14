4865885dfcaf01cc903111503ecf304cde8e32cb
accko199806 <haklee.googl@gmail.com>
2021-02-14 16:51:47 +0900

    정수 2개 입력받아 합 출력하기1

---
+++ CodeUp/1000/1038.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    long sum;
+    cin >> a >> b;
+    sum = a;
+    sum += b;
+    cout << sum << endl;
+    return 0;
+}
