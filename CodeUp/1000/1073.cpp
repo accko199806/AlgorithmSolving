243f885dfba086025a9e3c9a167928ba24662d46
accko199806 <haklee.googl@gmail.com>
2021-02-15 23:59:54 +0900

    0 입력될 때까지 무한 출력하기2

---
+++ CodeUp/1000/1073.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    while (true) {
+        int m;
+        cin >> m;
+        if (m == 0) break;
+        cout << m << endl;
+    }
+    return 0;
+}
