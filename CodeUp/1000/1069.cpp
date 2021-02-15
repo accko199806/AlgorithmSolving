05ef61f0a6e1ffb5ad2746c8d6fb6a2e4b1236ac
accko199806 <haklee.googl@gmail.com>
2021-02-15 15:49:39 +0900

    평가 입력받아 다르게 출력하기

---
+++ CodeUp/1000/1069.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char c;
+    cin >> c;
+    if (c == 'A') cout << "best!!!" << endl;
+    else if (c == 'B') cout << "good!!" << endl;
+    else if (c == 'C')cout << "run!" << endl;
+    else if (c == 'D')cout << "slowly~" << endl;
+    else cout << "what?" << endl;
+    return 0;
+}
