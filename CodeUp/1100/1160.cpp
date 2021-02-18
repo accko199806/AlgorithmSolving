a464c592cb9b16fc08c9043c24d5994c0762bfb7
accko199806 <haklee.googl@gmail.com>
2021-02-18 23:05:30 +0900

    아르바이트 가는 날

---
+++ CodeUp/1100/1160.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    cout << (n == 1 || n == 3 || n == 5 || n == 7 ? "oh my god" : "enjoy") << endl;
+    return 0;
+}
