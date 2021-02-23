e82095712d01e8d09e5ffde455a8e9cda08568bb
accko199806 <haklee.googl@gmail.com>
2021-02-23 14:32:46 +0900

    수열의 값 구하기

---
+++ CodeUp/1200/1269.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, c, n;
+    cin >> a >> b >> c >> n;
+    for (int i = 2; i <= n; i++) a = (a * b) + c;
+    cout << a << endl;
+    return 0;
+}
